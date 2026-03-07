c58598298902f375148a7cfff03ccea0b52b2ed8
Raquel <raquelmartiniano14@gmail.com>
2026-03-07 00:10:24 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+
+        int idade = 21;
+        double altura = 1.75;
+        String nome = "Raquel";
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+}
